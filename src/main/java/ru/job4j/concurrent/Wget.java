175bee51a61537ff46948396b7b061e025f95ee6
vadimstr102 <vadimstr102@gmail.com>
2021-05-12 15:05:00 +0700

    1.2. Режим ожидания. [#530072]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        try {
+                            System.out.print("\rLoading: " + i + "%");
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+}
