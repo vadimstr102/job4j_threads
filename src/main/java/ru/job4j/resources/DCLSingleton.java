11a1109f56fa48fe4be9d130c0eb1168e0b3092c
vadimstr102 <vadimstr102@gmail.com>
2021-05-14 16:53:50 +0700

    2. Модель памяти Java [#530078]

---
+++ src/main/java/ru/job4j/resources/DCLSingleton.java
@@ -0,0 +1,19 @@
+package ru.job4j.resources;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    private DCLSingleton() {
+    }
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}
